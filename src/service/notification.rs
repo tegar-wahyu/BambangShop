98bfb6411120485dcd3729fe1c2be04e154df6ff
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-04-15 13:18:26 +0700

    Implement publish function in Program service and Program controller.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -5,7 +5,7 @@ use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
-use crate::repository::subscriber::{self, SubscriberRepository};
+use crate::repository::subscriber::SubscriberRepository;
 
 pub struct NotificationService;
 
